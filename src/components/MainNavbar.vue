701f5ccd160489e96da844ad09d8bf3a60686820
yousaf-demo <you2531995@gmail.com>
2025-03-22 16:12:26 +0500

    navbar updated

--- src/components/MainNavbar.vue
+++ src/components/MainNavbar.vue
@@ -1,30 +1,43 @@
 <template>
 <section class="main-navbar">
     <div class="container">
-        <div class="navbar-wrapper">
+        <nav class="navbar navbar-expand-lg">
+      <!-- Logo -->
+      <a class="navbar-brand" href="#">
+        <div class="logo-wrapper">
+        <img src="../assets/logo.png" alt="Logo">
+    </div>
+      </a>
 
-            <ul class="d-flex justify-content-between list-unstyled">
-                <li>
-                    <div class="logo-wrapper">
-                        <img src="../assets/logo.png" alt="">
-                    </div>
-                </li>
-                <li>
-                    <ul class="d-flex gap-4 list-unstyled">
-                        <li>
-                            <a href="#">Home</a>
-                        </li>
-                        <li>
-                            <a href="#">Work</a>
-                        </li>
-                        <li>
-                            <a href="#">Contact</a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
+      <!-- Toggle Button for Mobile View -->
+      <button 
+        class="navbar-toggler" 
+        type="button" 
+        data-bs-toggle="collapse" 
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false" 
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-        </div>
+      <!-- Navbar Links -->
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Work</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+   
+  </nav>
     </div>
 
 </section>
@@ -34,9 +47,3 @@ export default {
     name: 'MainNavbar'
 }
 </script>
-<style>
-.navbar-wrapper .logo-wrapper{
-    width: 50px;
-    height: 50px;
-}
-</style>
